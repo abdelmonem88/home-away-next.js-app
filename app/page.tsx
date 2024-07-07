4edcd670dc89cf450048f334a0df5cccd454afde
abdelmonem88 <abdelmonem1888@gmail.com>
2024-07-07 21:26:51 +0300

    add the below chnages: - finalize navbar by adding the links dropdwoen component - add clerk for authentication - add form reusable components (FormInput-FormContainer-SubmitButton)

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ function HomePage() {
   return (
     <div>
       <h1 className="text-3xl">HomePage</h1>
-      <Button variant="default" size="lg" className="capitalize m-8">
-        Click me
-      </Button>
     </div>
   );
 }

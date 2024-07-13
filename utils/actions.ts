fa7250c64f570c8b4aee40d98eaf6d1ffcdf2fde
abdelmonem88 <abdelmonem1888@gmail.com>
2024-07-13 15:01:04 +0300

    add below chnages: - add update profile image funcationality - start adding property page

--- utils/actions.ts
+++ utils/actions.ts
@@ -4,9 +4,10 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-import { profileSchema } from "./schemas";
+import { imageSchema, profileSchema, propertySchema } from "./schemas";
 import db from "./db";
 import { validateWithZodSchema } from "./schemas";
+import { uploadImage } from "./supabase";
 
 export const createProfileAction = async (
   prevState: any,
@@ -104,4 +105,43 @@ export const updateProfileAction = async (
   } catch (error) {
     return renderError(error);
   }
+};
+
+export const updateProfileImageAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  const user = await getAuthUser();
+  try {
+    const image = formData.get("image") as File;
+    const validatedFields = validateWithZodSchema(imageSchema, { image });
+    const fullPath = await uploadImage(validatedFields.image);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: {
+        profileImage: fullPath,
+      },
+    });
+    revalidatePath("/profile");
+    return { message: "Profile image updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+export const createPropertyAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect("/");
 };
